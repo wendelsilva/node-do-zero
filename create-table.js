3d4ce53208d8589f155eb338a9e856024f494f53
wendel <wendel245.ws@gmail.com>
2023-11-17 22:30:41 -0300

    remove comments

--- create-table.js
+++ create-table.js
@@ -1,9 +1,5 @@
 import { sql } from './db.js'
 
-// sql`DROP TABLE IF EXISTS videos`.then(() => {
-//     console.log('tabela deletada');
-// })
-
 sql`
     CREATE TABLE videos (
         id TEXT PRIMARY KEY,
